4418807c7232d43b40fa06eabe4113a03ba01d58
Lynn Gammie <lynn@grndwrk.ca>
2018-01-04 23:12:38 -0500

    More submission form styling

--- cofowp/wp-content/themes/cofodev/page-submit.php
+++ cofowp/wp-content/themes/cofodev/page-submit.php
@@ -25,6 +25,18 @@ get_header(); ?>
 				$formcode = get_field('submission_form_shortcode');
 				echo do_shortcode($formcode); ?>
 			</div>
+			<div class="challenge-share anm-container fadein">
+				<h4 class="anm-item slideright-item">Share the challenge</h4>
+				<div class="social-nav anm-item slideright-item">
+					<ul>
+						<li>
+							<?php global $wp;
+							$url = home_url( $wp->request ); ?>
+							<a class="social-share" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo $url; ?>" target="_blank"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+						<li><a class="social-share" href="https://twitter.com/intent/tweet?text=Hello%20world" data-size="large" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+					</ul>
+				</div>
+			</div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
