dcb84a79587029cf5f1d8958efc714b8b1724ab2
Lynn Gammie <lynn@grndwrk.ca>
2018-01-12 09:51:51 -0500

    404 redirect to home

--- cofowp/wp-content/themes/cofodev/404.php
+++ cofowp/wp-content/themes/cofodev/404.php
@@ -7,23 +7,7 @@
  * @package ndrscrs
  */
 
-get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'ndrscrs' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
-get_footer();
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: ".get_bloginfo('url'));
+	exit();
+?>
