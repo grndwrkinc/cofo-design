61f2b4bc16abd4de2418a938c42979c9b80fd9fa
Pramesh Attwala <pramesh@grndwrk.ca>
2018-04-02 17:16:54 -0400

    Make sure that when a product's variants are changed on Shopify (i.e. when a new Variant is added), that it appears in the WordPress backend for existing products.

--- cofowp/wp-content/plugins/grndwrk-shopify/js/admin.js
+++ cofowp/wp-content/plugins/grndwrk-shopify/js/admin.js
@@ -1,6 +1,9 @@
 var gwsh = {};
 
 gwsh.init = function() {
+
+	gwsh.getProductVariants();
+
 	$('.select-product').on('click', gwsh.selectProduct);
 	$('.edit-product').on('click', gwsh.editProductSelection);
 	
@@ -23,18 +26,24 @@ gwsh.init = function() {
 		var $el = $(this);
 		var variants = $.parseJSON($("#gwsh_product_variants").val());
 		var $field = acf.get_closest_field( $el, 'repeater' ); // ACF magic
-		var $select = $field.find('.acf-input > select');
-		//On a new Product, when you click 'Add Row' the first time
-		// an empty <select> field will be added to the page. We need to
-		// populate it with the variants as <option> fields.
-		// We only need to do this once because the next row 
-		// (when 'Add Row' is clicked) will be 'cloned' by ACF.
-		if($select.length == 1 && $select.find('option').length == 0) {
+		var $select = $field.find('.acf-table .acf-input > select');
+
+		// On a new Product, when you click 'Add Row' a <select> field will 
+		// be added to the page. We need to populate it with the variants as 
+		// <option> elements.
+
+		//Grab the last <select> for the field anc compare the # of options
+		// to the # of variants we sould have. If they're different, we need
+		// to update the <option>s to match.
+		if($select.last().find('option').length !== variants.length) {	
 			for(var i=0; i< variants.length; i++) {
-				$select.append($('<option>',{
-					value: variants[i].product_id + "_" + variants[i].id,
-					text: variants[i].title
-				}));
+				if($select.last().find('option').text() === variants[i].title) { /* Do nothing. Ignore a match to avoid duplicates. */}
+				else {
+					$select.append($('<option>',{
+						value: variants[i].product_id + "_" + variants[i].id,
+						text: variants[i].title
+					}));
+				}
 			}
 		}
 	});
@@ -73,36 +82,40 @@ gwsh.selectProduct = function() {
 			.text('Edit')
 			.on('click', gwsh.editProductSelection);
 
-		//Use AJAX to make an API call to get the
-		// product variants
-		$.ajax({
-			url: ajaxurl,
-			type: 'POST',
-			async: false,
-			cache: false,
-			data: {
-				action: 'gwsh_get_product_variants',
-				productID: $("#gwsh_product_id").val()
-			},
-
-			// Send the variants back to the page
-			success: function(response) {
-
-				var data = $.parseJSON(response);
-				var variants = [];
-				for(var i=0; i < data.length; i++) {
-					variants[i] = { 
-						id: data[i].id, 
-						product_id: data[i].product_id,
-						title: data[i].title
-					};
-				}
-				
-				//Store them in the hidden field for saving
-				$("#gwsh_product_variants").val(JSON.stringify(variants));
-			}
-		});
+		gwsh.getProductVariants();
 	}
+}
+
+gwsh.getProductVariants = function() {
+	//Use AJAX to make an API call to get the
+	// product variants
+	$.ajax({
+		url: ajaxurl,
+		type: 'POST',
+		async: false,
+		cache: false,
+		data: {
+			action: 'gwsh_get_product_variants',
+			productID: $("#gwsh_product_id").val()
+		},
+
+		// Send the variants back to the page
+		success: function(response) {
+
+			var data = $.parseJSON(response);
+			var variants = [];
+			for(var i=0; i < data.length; i++) {
+				variants[i] = { 
+					id: data[i].id, 
+					product_id: data[i].product_id,
+					title: data[i].title
+				};
+			}
+			
+			//Store them in the hidden field for saving
+			$("#gwsh_product_variants").val(JSON.stringify(variants));
+		}
+	});
 }
 
 gwsh.setTitle = function(selected) {
